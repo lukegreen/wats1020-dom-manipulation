16fde48600f9e7ae32af18b8c7579eedd00f0aec
elukuent <elukuent@gmail.com>
2016-04-22 19:19:10 -0400

    basic requirements

--- js/main.js
+++ js/main.js
@@ -22,7 +22,11 @@ $( document ).ready(function() {
     //      2. Fill the user's first and last name into `div.user-info`.
     //      (NOTE: You do not have to perform any validation on the data as
     //          a base requirement.)
-
+  
+  $('#login-form a').on('click', function(){
+    $('#login-form').hide(400);
+    $('.user-info').show();
+      });
 
     // TODO: Create a function to listen for clicks on all the "View Details"
     // buttons so that when a user clicks a "View Details" button they see
@@ -33,6 +37,21 @@ $( document ).ready(function() {
     //      3. Toggle visibility of all the elements within that parent with the class `details`.
     //      4. Change the text of the "view details" button to read "hide details" so the user
     //          understands they can hide the text again.
+  
+  $('.view-details').on('click', function(event){
+    var targetElement = event.target;
+    var container = targetElement.parentElement.parentElement;
+    $(container).find('.details').each(function (index, el){
+      if($(el).is(':visible')) {
+        $(el).fadeOut();
+        targetElement.innerHTML = "View Details";
+      } else {
+        $(el).fadeIn();
+        targetElement.innerHTML = "Hide Details";
+      }
+    });
+  });
+  
 
     // TODO: Create a function that listens for clicks on the voting buttons and
     // looks at the `data-vote` attribute on each button to see what was voted for,
@@ -43,5 +62,23 @@ $( document ).ready(function() {
     //      3. Increment the counter for whichever vote talley is affected.
     //      4. Determine the respective percentages (out of 100) for each progress bar.
     //      5. Modify the `width` attribute on each progress bar to set the updated percentage.
+  
+   $('.vote').on('click', function(event){
+    var targetEl = $(event.target);
+    var targetVote = $(targetEl).attr('data-vote');
+    if (targetVote === "great") {
+      voteCounts.great = voteCounts.great + 1;  
+    } else {
+      voteCounts.greatest = voteCounts.greatest + 1;
+    }
+     
+    voteCounts.total = voteCounts.great + voteCounts.greatest;   
+    var percentGreat = (voteCounts.great / voteCounts.total) * 100;
+    var percentGreatest = (voteCounts.greatest / voteCounts.total) * 100;
+    
+    $('.great-progress').attr('style', 'width:' + percentGreat + '%'); 
+    $('.greatest-progress').attr('style', 'width:' + percentGreatest + '%');
+    
+  })
 
 });
